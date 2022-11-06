1aec5f38c5b78f0b7993840610588df543d8dca6
mustafauysal <me@uysalmustafa.com>
2022-11-06 20:00:20 +0300

    Add uninstaller #44

---
+++ uninstall.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Uninstall Yandex Metrica
+ *
+ * Deletes all plugin related data and configurations
+ *
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit;
+}
+
+require_once 'yandex-metrica.php';
+
+delete_option( 'metrica_options' );
